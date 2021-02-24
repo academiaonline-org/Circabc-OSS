fd7a85eb1961489d35c67c9db6590cb8332110bf
filips <slobodan.filipovic@ext.ec.europa.eu>
2021-02-24 12:30:42 +0100

    add new test

--- circabc-e2e/cypress/integration/circabc/user/create-page.spec.js
+++ circabc-e2e/cypress/integration/circabc/user/create-page.spec.js
@@ -6,9 +6,8 @@ describe("Create users", function () {
     );
   });
 
-   /*
-   Admin users are created in the docker images in oss version
-
+   
+   
    it("successfully created category admin", function () {
     cy.visit("/me/roles", {
       failOnStatusCode: false,
@@ -43,26 +42,8 @@ describe("Create users", function () {
 
     cy.get("[data-cy=create]").click();
     cy.contains("Success");
-  });*/
-
-
-  it("successfully created interest group admin", function () {
-    for (var i = 1; i < 5; i++) {
-      cy.visit("/me/roles", {
-        failOnStatusCode: false,
-      });
-      cy.get("[data-cy=create-user]").click();
-      cy.get("[data-cy=username]").type(Cypress.env("user" + i + ".username"));
-      cy.get("[data-cy=firstname]").type(Cypress.env("user" + i + ".first.name"));
-      cy.get("[data-cy=lastname]").type(Cypress.env("user" + i + ".last.name"));
-      cy.get("[data-cy=email]").type(Cypress.env("user" + i + ".email"));
-      cy.get("[data-cy=phone]").type(Cypress.env("user" + i + ".phone"));
-      cy.get("[data-cy=postalAddress]").type(Cypress.env("user" + i + ".postalAddress"));
-      cy.get("[data-cy=password]").type(Cypress.env("user" + i + ".password"));
-      cy.get("[data-cy=passwordVerify]").type(Cypress.env("user" + i + ".password"));
-
-      cy.get("[data-cy=create]").click();
-      cy.contains("Success");
-    }
   });
+
+
+  
 });
