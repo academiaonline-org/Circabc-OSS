caaf79a26715688218fdf45ef62705ea5bd70f35
COLOMAR Sebastian (OLAF-EXT) - colomse <colomse@s-olaf-siep.olaf.cec.eu.int>
2021-03-23 10:32:16 +0100

    Fix prepare_release.sh

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -10,15 +10,19 @@ release=${1}                                                                    
 git checkout -b ${release}                                                      ;
 #################################################################################
 sed --in-place /branch=docker/s/docker/${release}/ README.md                    ;
-sed --in-place /image.*latest/s/latest/${release#v}/ etc/*/manifests/*.yaml     ;
+sed --in-place /circabc.*latest/s/latest/${release#v}/ 				\
+	circabc-docker/*/Dockerfile.* 						\
+	Dockerfile.* 								\
+	etc/*/manifests/*.yaml 							\
+										;
 sed --in-place s/-\ docker/-\ ${release}/ .github/workflows/ci.yaml             ;
-sed --in-place s/latest-maven/${release#v}-maven/ Dockerfile.*			;
 #################################################################################
-git add                                                                         \
-  README.md                                                                     \
-  etc/*/manifests/*.yaml                                                        \
-  .github/workflows/ci.yaml                                                     \
-  Dockerfile.* 									\
+git add 									\
+	.github/workflows/ci.yaml 						\
+	circabc-docker/*/Dockerfile.* 						\
+	Dockerfile.* 								\
+	etc/*/manifests/*.yaml 							\
+	README.md 								\
                                                                                 ;
 #################################################################################
 git commit -m ${release}                                                        ;
