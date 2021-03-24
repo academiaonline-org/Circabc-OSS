cdae32423aef5359ee4e7f0880b9db4b3693d502
COLOMAR Sebastian (OLAF-EXT) - colomse <colomse@s-olaf-siep.olaf.cec.eu.int>
2021-03-24 10:13:31 +0100

    Cosmetic

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -10,13 +10,14 @@ release=${1}                                                                    
 #################################################################################
 git checkout -b ${release}                                                      ;
 #################################################################################
-sed --in-place /branch=${main_branch}/s/${main_branch}/${release}/ README.md	;
+sed --in-place s/-\ ${main_branch}/-\ ${release}/ .github/workflows/ci.yaml	;
 sed --in-place /circabc.*latest/s/latest/${release#v}/ 				\
 	circabc-docker/*/Dockerfile.* 						\
 	Dockerfile.* 								\
 	etc/*/manifests/*.yaml 							\
 										;
-sed --in-place s/-\ ${main_branch}/-\ ${release}/ .github/workflows/ci.yaml	;
+sed --in-place /branch=${main_branch}/s/${main_branch}/${release}/ README.md	;
+sed --in-place s/${main_branch}/${release/ var/spool/username			;
 #################################################################################
 git add 									\
 	.github/workflows/ci.yaml 						\
@@ -24,6 +25,7 @@ git add 									\
 	Dockerfile.* 								\
 	etc/*/manifests/*.yaml 							\
 	README.md 								\
+	var/spool/username 							\
                                                                                 ;
 #################################################################################
 git commit -m ${release}                                                        ;
@@ -31,4 +33,6 @@ git commit -m ${release}                                                        
 git push --set-upstream origin ${release}                                       ;
 #################################################################################
 git checkout ${main_branch}							;
+#################################################################################
+git branch -D ${release}							;
 #################################################################################
