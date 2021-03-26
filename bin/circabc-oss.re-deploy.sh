03e7b648d2a603fecb61fabc15345a735fcbce69
COLOMAR Sebastian (OLAF-EXT) - colomse <colomse@s-olaf-siep.olaf.cec.eu.int>
2021-03-26 10:18:56 +0100

    Fix nightly redeployment

--- bin/circabc-oss.re-deploy.sh
+++ bin/circabc-oss.re-deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -x
+#################################################################################
 sudo docker stack rm ${stack}
 sleep 100
 cd ${HOME}/${stack}
 git checkout ${tag}
 sudo docker stack deploy -c ${HOME}/${stack}/etc/swarm/manifests/${stack}.yaml ${stack}
+git checkout ${main_branch}
+#################################################################################
