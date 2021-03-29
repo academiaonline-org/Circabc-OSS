ea2d59348937b7be90af82c2cd207ae3acad8dbd
COLOMAR Sebastian (OLAF-EXT) - colomse <colomse@s-olaf-siep.olaf.cec.eu.int>
2021-03-29 08:10:39 +0200

    Fix re-deployment

--- bin/re-deploy.sh
+++ bin/re-deploy.sh
@@ -3,7 +3,10 @@
 sudo docker stack rm ${stack}
 sleep 100
 cd ${HOME}/${stack}
+previous_tag=$( git status | awk '/On branch/ { print $4 }' )
+git stash
 git checkout ${tag}
 sudo docker stack deploy -c ${HOME}/${stack}/etc/swarm/manifests/${stack}.yaml ${stack}
-git checkout ${main_branch}
+git checkout ${previous_tag}
+git stash apply
 #################################################################################
