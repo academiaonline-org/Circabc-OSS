98643e6af0ea5c63ff1aa677599c8317bef7d663
COLOMAR Sebastian (OLAF-EXT) - colomse <colomse@s-olaf-siep.olaf.cec.eu.int>
2021-03-29 09:46:37 +0200

    Fix git stash process in re-deploy.sh

--- bin/re-deploy.sh
+++ bin/re-deploy.sh
@@ -4,9 +4,10 @@ sudo docker stack rm ${stack}
 sleep 100
 cd ${HOME}/${stack}
 previous_tag=$( git status | awk '/On branch/ { print $4 }' )
-git stash
+git stash save
 git checkout ${tag}
 sudo docker stack deploy -c ${HOME}/${stack}/etc/swarm/manifests/${stack}.yaml ${stack}
 git checkout ${previous_tag}
 git stash apply
+git stash drop
 #################################################################################
