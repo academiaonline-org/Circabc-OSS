892b3646a52d1619979a5277ab19fbf133c4a82e
Sebastian Colomar <sebastian.colomar@gmail.com>
2023-03-15 09:31:34 +0000

    Docker/

---
+++ Docker/bin/prepare_release.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#################################################################################
+#       Copyright (C) 2020        Sebastian Francisco Colomar Bauza             #
+#       SPDX-License-Identifier:  GPL-2.0-only                                  #
+#################################################################################
+set -x                                                                          ;
+#################################################################################
+main_branch=docker								;
+release=${1}                                                                    ;
+#################################################################################
+git checkout -b ${release}                                                      ;
+#################################################################################
+sed --in-place 									\
+	s/-\ ${main_branch}/-\ ${release}/ 					\
+	.github/workflows/ci.yaml						;
+sed --in-place 									\
+	/circabc.*latest/s/latest/${release#v}/ 				\
+	circabc-docker/*/Dockerfile.* 						\
+	Dockerfile.* 								\
+	etc/*/manifests/*.yaml 							\
+										;
+sed --in-place 									\
+	/branch=${main_branch}/s/${main_branch}/${release}/ 			\
+	README.md 								\
+										;
+sed --in-place 									\
+	s/${main_branch}/${release}/ 						\
+	var/spool/username							;
+#################################################################################
+git add                                                                         \
+	.github/workflows/ci.yaml 						\
+	circabc-docker/*/Dockerfile.* 						\
+	Dockerfile.* 								\
+	etc/*/manifests/*.yaml 							\
+	README.md 								\
+	var/spool/username 							\
+                                                                                ;
+#################################################################################
+git commit -m ${release}                                                        ;
+#################################################################################
+git push --set-upstream origin ${release}                                       ;
+#################################################################################
+git checkout ${main_branch}							;
+#################################################################################
+git branch -D ${release}							;
+#################################################################################
+set +x                                                                          ;
+#################################################################################
---
+++ Docker/bin/re-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+#################################################################################
+sudo docker stack rm ${stack}
+sleep 100
+cd ${HOME}/${stack}
+previous_tag=$( git status | awk '/On branch/ { print $4 }' )
+git stash save
+git checkout ${tag}
+sudo docker stack deploy -c ${HOME}/${stack}/etc/swarm/manifests/${stack}.yaml ${stack}
+git checkout ${previous_tag}
+git stash apply
+git stash drop
+#################################################################################
